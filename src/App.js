3a15bd7b4b3c76c02b4488680bafc75e9ab8b349
yusuf <yusufgad24@gmail.com>
2023-09-23 19:36:24 +0300

    react-app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Header from "./Header";
-import Main from "./Main";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 export default function App() {
   return (
