1bfd6eba9194c65ddf9df532d022aecddd6049ea
yusuf <yusufgad24@gmail.com>
2023-09-24 11:55:04 +0300

    handling answers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,94 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
+import { useEffect, useReducer } from "react";
+
+import Loader from "./components/UI/Loader";
+import Error from "./components/UI/Error";
+
+import Header from "./components/Layout/Header";
+import Main from "./components/Layout/Main";
+
+import StartScreen from "./components/UI/StartScreen";
+import Question from "./components/Question/Question";
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+
+    case "newAnswer":
+      const currentQuestion = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === currentQuestion.correctOption
+            ? state.points + currentQuestion.points
+            : state.points,
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 export default function App() {
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+
+        {status === "ready" && (
+          <StartScreen dispatch={dispatch} numQuestions={numQuestions} />
+        )}
+        {status === "active" && (
+          <Question
+            dispatch={dispatch}
+            question={questions[index]}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
---
+++ src/components/Question/Options.js
@@ -0,0 +1,26 @@
+function Options({ question, answer, dispatch }) {
+  const hasAnswered = answer !== null;
+
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          disabled={answer !== null}
+          key={option}
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Options;
